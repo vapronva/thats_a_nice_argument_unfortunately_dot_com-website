84ba7cf84dab8df34b0a194f0c30b47decf80f3e
Vladimir (vapronva) <vapronhm@pm.me>
2022-02-09 01:35:03 +0300

    fffff

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd api/
-docker build -t thats-a-nice-argument-unfortunately-dot-com-api:${CI_COMMIT_TAG} .
+docker build -t thats-a-nice-argument-unfortunately-dot-com-api:${CI_COMMIT_TAG:-$CI_COMMIT_SHA} .
 cd ../
 docker-compose -f 'docker-compose.yml' --project-name 'wa-tnauc' down
 docker-compose -f 'docker-compose.yml' --project-name 'wa-tnauc' up -d
