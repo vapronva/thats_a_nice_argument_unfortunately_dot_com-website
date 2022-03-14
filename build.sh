91e81cdc9d488dd7adbaf6c404298ca45d325985
Vladimir (vapronva) <vapronhm@pm.me>
2022-03-15 00:00:08 +0300

    Add website scripts & update CI configuration for the website

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@ set -e
 cd api/
 docker build -t thats-a-nice-argument-unfortunately-dot-com-api:${CI_COMMIT_TAG:-$CI_COMMIT_SHA} .
 cd ../
+cd web/
+docker build -t thats-a-nice-argument-unfortunately-dot-com-web:${CI_COMMIT_TAG:-$CI_COMMIT_SHA} .
+cd ../
 IMAGE_VERSION=${CI_COMMIT_TAG:-$CI_COMMIT_SHA} docker-compose -f 'docker-compose.yml' --project-name 'wa-tnauc' down
 IMAGE_VERSION=${CI_COMMIT_TAG:-$CI_COMMIT_SHA} docker-compose -f 'docker-compose.yml' --project-name 'wa-tnauc' up -d
---
+++ web/main.py
@@ -0,0 +1,27 @@
+from ipaddress import IPv4Address, AddressValueError
+from flask import Flask, render_template, jsonify, request
+
+app = Flask("web-thats_a_nice_argument_unfortunately_dot_com")
+
+@app.errorhandler(404)
+def error_404(e):
+    return jsonify({"error":{"name":"PATH_NOT_FOUND","description":"The requested path was not found."},"result": None}), 404
+
+@app.errorhandler(500)
+def error_500(e):
+    return jsonify({"error":{"name":"ISE","description":"Internal server error occurred. That's really fricking bad!"},"result": None}), 500
+
+@app.route("/")
+def main_root():
+    return render_template("index.html")
+
+@app.route("/ipp")
+def main_ipp():
+    try:
+        userIP = IPv4Address(request.headers.get("X-Forwarded-For"))
+    except AddressValueError:
+        userIP = IPv4Address(
+            request.headers.get("X-Forwarded-For").split(", ")[0])
+    return render_template("ipp.html", userIP=str(userIP))
+
+app.run(debug=False, host="0.0.0.0", port=8000)
