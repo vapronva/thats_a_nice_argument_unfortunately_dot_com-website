68cb3a66719344e96d0e499e5118c65fdadd6e6c
Vladimir (vapronva) <vapronhm@pm.me>
2022-05-14 03:26:51 +0300

    More small fixes in IPD fetcher JS

--- web/static/javascript/ipd.js
+++ web/static/javascript/ipd.js
@@ -1,5 +1,13 @@
 // Original code by gavra on https://codepen.io/gavra/pen/tEpzn; modified by vapronva for https://thats-a-nice-argument-unfortunately.com
 
+var startPositionArray = 0; // skipcq: JS-0239
+var currentTextPosition = 0; // skipcq: JS-0239
+var contentOfaLine = ""; // skipcq: JS-0239
+const eachCharachterDelay = 23;
+const maxLinesAtTime = 50;
+const nextLineDelay = 230;
+const startTimeDelay = 2200;
+
 fetch("https://api.thats-a-nice-argument-unfortunately.com/v1/ip")
     .then(res => res.json())
     .then(data => {
@@ -7,16 +15,8 @@ fetch("https://api.thats-a-nice-argument-unfortunately.com/v1/ip")
         textArrayLength = originalTextArray[0].length;
     });
 
-const eachCharachterDelay = 23;
-var startPositionArray = 0; // skipcq: JS-0239
-const maxLinesAtTime = 50;
-var currentTextPosition = 0; // skipcq: JS-0239
-var contentOfaLine = ""; // skipcq: JS-0239
-const nextLineDelay = 230;
-const startTimeDelay = 2200;
-
 function typewriter() { // skipcq: JS-0128
-    let destination = document.getElementById("typedtext");
+    const destination = document.getElementById("typedtext");
     contentOfaLine = " ";
     currentRow = Math.max(0, startPositionArray - maxLinesAtTime);
     while (currentRow < startPositionArray) {
