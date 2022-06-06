ad70203de82318b9cc863c5502b287dd4fd744a5
Vladimir (vapronva) <vapronhm@pm.me>
2022-06-06 13:04:21 +0300

    Fix some anti-pattern thingies + linting at JS

--- scripts/prepareAndPushStorage.sh
+++ scripts/prepareAndPushStorage.sh
@@ -1,37 +1,41 @@
 #!/bin/bash
 
-TMP_DIR_NAME=/tmp/tnaudc-`date +%s`
-mkdir -p $TMP_DIR_NAME
+TMP_DIR_NAME=/tmp/tnaudc-$(date +%s)
+mkdir -p "$TMP_DIR_NAME"
 echo "Created temporary directory $TMP_DIR_NAME"
 
-cp -r web/static/ $TMP_DIR_NAME
+cp -r "web/static/" "$TMP_DIR_NAME"
 echo "Copied static files to $TMP_DIR_NAME"
 
-mkdir -p $TMP_DIR_NAME/css/_/`cat version.txt`
-mv $TMP_DIR_NAME/css/style.css $TMP_DIR_NAME/css/_/`cat version.txt`/style.css
-echo "Moved style.css to $TMP_DIR_NAME/css/_/`cat version.txt`/style.css"
+mkdir -p "$TMP_DIR_NAME/css/_/$(cat version.txt)"
+mv "$TMP_DIR_NAME/css/style.css" "$TMP_DIR_NAME/css/_/$(cat version.txt)/style.css"
+echo "Moved style.css to $TMP_DIR_NAME/css/_/$(cat version.txt)/style.css"
 
-mkdir -p $TMP_DIR_NAME/javascript/_/`cat version.txt`
-mv $TMP_DIR_NAME/javascript/ipd.js $TMP_DIR_NAME/javascript/_/`cat version.txt`/ipd.js
-echo "Moved ipd.js to $TMP_DIR_NAME/javascript/_/`cat version.txt`/ipd.js"
+mkdir -p "$TMP_DIR_NAME/javascript/_/$(cat version.txt)"
+mv "$TMP_DIR_NAME/javascript/ipd.js" "$TMP_DIR_NAME/javascript/_/$(cat version.txt)/ipd.js"
+echo "Moved ipd.js to $TMP_DIR_NAME/javascript/_/$(cat version.txt)/ipd.js"
 
-find $TMP_DIR_NAME -name "*.css" -exec uglifycss --output {}.min {} \;
-for f in `find $TMP_DIR_NAME -name "*.css.min"`; do mv $f ${f/.css.min/}.min.css; done
+find "$TMP_DIR_NAME" -name "*.css" -exec uglifycss --output "{}.min" "{}" \;
+while IFS= read -r -d "" file; do
+    mv "$file" "${file/.css.min/}.min.css"
+done < <(find "$TMP_DIR_NAME" -name "*.css.min" -print0)
 echo "Minified CSS files"
 
-find $TMP_DIR_NAME -name "*.js" -exec uglifyjs --compress --mangle --output {}.min {} \;
-for f in `find $TMP_DIR_NAME -name "*.js.min"`; do mv $f ${f/.js.min/}.min.js; done
+find "$TMP_DIR_NAME" -name "*.js" -exec uglifyjs --compress --mangle --output "{}.min" "{}" \;
+while IFS= read -r -d "" file; do
+  mv "$file" "${file/.js.min/}.min.js"
+done < <(find "$TMP_DIR_NAME" -name "*.js.min" -print0)
 echo "Minified JS files"
 
-wget https://static.video.vapronva.pw/_websites/thats-a-nice-argument-unfortunately-com/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4 -O $TMP_DIR_NAME/video/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4
-wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-5.jpeg -O $TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-5.jpeg
-wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-6.jpeg -O $TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-6.jpeg
-wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-7.jpeg -O $TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-7.jpeg
-wget https://static.images.vapronva.pw/mtch-wtr-bttl/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg -O $TMP_DIR_NAME/images/backgrounds/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg
+wget https://static.video.vapronva.pw/_websites/thats-a-nice-argument-unfortunately-com/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4 -O "$TMP_DIR_NAME/video/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4"
+wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-5.jpeg -O "$TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-5.jpeg"
+wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-6.jpeg -O "$TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-6.jpeg"
+wget https://static.images.vapronva.pw/ilya-photos/iomp/ivobl-ph-il-7.jpeg -O "$TMP_DIR_NAME/images/backgrounds/iomp/ivobl-ph-il-7.jpeg"
+wget https://static.images.vapronva.pw/mtch-wtr-bttl/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg -O "$TMP_DIR_NAME/images/backgrounds/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg"
 echo "Downloaded large videos and images"
 
-rclone copy $TMP_DIR_NAME/ slctl-sttctnaudc:static_thatsaniceargumentunfortunately_com-7e358dffde76
+rclone copy "$TMP_DIR_NAME/" slctl-sttctnaudc:static_thatsaniceargumentunfortunately_com-7e358dffde76
 echo "Pushed to Selectel Storage"
 
-rm -rf $TMP_DIR_NAME
+rm -rf "$TMP_DIR_NAME"
 echo "Deleted temporary directory $TMP_DIR_NAME"
--- web/static/javascript/ipd.js
+++ web/static/javascript/ipd.js
@@ -1,38 +1,38 @@
 // Original code by gavra on https://codepen.io/gavra/pen/tEpzn; modified by vapronva for https://thats-a-nice-argument-unfortunately.com
 
-var startPositionArray = 0; // skipcq: JS-0239
-var currentTextPosition = 0; // skipcq: JS-0239
-var contentOfaLine = ""; // skipcq: JS-0239
+let startPositionArray = 0; // skipcq: JS-0239
+let currentTextPosition = 0; // skipcq: JS-0239
+let contentOfaLine = ''; // skipcq: JS-0239
 const eachCharachterDelay = 23;
 const maxLinesAtTime = 50;
 const nextLineDelay = 230;
 const startTimeDelay = 2200;
 
-fetch("https://api.thats-a-nice-argument-unfortunately.com/v1/ip")
-    .then(res => res.json())
-    .then(data => {
-        originalTextArray = data.result.final_list;
-        textArrayLength = originalTextArray[0].length;
-    });
+fetch('https://api.thats-a-nice-argument-unfortunately.com/v1/ip')
+  .then((res) => res.json())
+  .then((data) => {
+    originalTextArray = data.result.final_list;
+    textArrayLength = originalTextArray[0].length;
+  });
 
 function typewriter() { // skipcq: JS-0128
-    const destination = document.getElementById("typedtext");
-    contentOfaLine = " ";
-    currentRow = Math.max(0, startPositionArray - maxLinesAtTime);
-    while (currentRow < startPositionArray) {
-        contentOfaLine += `${originalTextArray[currentRow++]}<br/>`;
+  const destination = document.getElementById('typedtext');
+  contentOfaLine = ' ';
+  currentRow = Math.max(0, startPositionArray - maxLinesAtTime);
+  while (currentRow < startPositionArray) {
+    contentOfaLine += `${originalTextArray[currentRow++]}<br/>`;
+  }
+  destination.innerHTML = `${contentOfaLine}${originalTextArray[startPositionArray].substring(0, currentTextPosition)}_`;
+  if (currentTextPosition++ === textArrayLength) {
+    currentTextPosition = 0;
+    startPositionArray++;
+    if (startPositionArray !== originalTextArray.length) {
+      textArrayLength = originalTextArray[startPositionArray].length;
+      setTimeout(typewriter, nextLineDelay);
     }
-    destination.innerHTML = `${contentOfaLine}${originalTextArray[startPositionArray].substring(0, currentTextPosition)}_`;
-    if (currentTextPosition++ === textArrayLength) {
-        currentTextPosition = 0;
-        startPositionArray++;
-        if (startPositionArray !== originalTextArray.length) {
-            textArrayLength = originalTextArray[startPositionArray].length;
-            setTimeout(typewriter, nextLineDelay);
-        }
-    } else {
-        setTimeout(typewriter, eachCharachterDelay);
-    }
+  } else {
+    setTimeout(typewriter, eachCharachterDelay);
+  }
 }
 
 setTimeout(typewriter, startTimeDelay);
