1dab67ab3f03703e415fc268158599da358e4b36
Vladimir (vapronva) <git@vprw.ru>
2023-12-02 05:40:02 +0300

    fix+chore(ci): rework storage pusher; remove dast

--- scripts/prepareAndPushStorage.sh
+++ scripts/prepareAndPushStorage.sh
@@ -7,26 +7,34 @@ echo "Created temporary directory ${TMP_DIR_NAME}"
 cp -r web/static/* "${TMP_DIR_NAME}"
 echo "Copied static files to ${TMP_DIR_NAME}"
 
-mkdir -p "${TMP_DIR_NAME}/css/_/$(cat version.txt)" || true
-mv "${TMP_DIR_NAME}/css/style.css" "${TMP_DIR_NAME}/css/_/$(cat version.txt)/style.css" || true
-echo "Moved style.css to ${TMP_DIR_NAME}/css/_/$(cat version.txt)/style.css" || true
+VERSION=$(<version.txt)
 
-mkdir -p "${TMP_DIR_NAME}/javascript/_/$(cat version.txt)" || true
-mv "${TMP_DIR_NAME}/javascript/ipd.js" "${TMP_DIR_NAME}/javascript/_/$(cat version.txt)/ipd.js" || true
-echo "Moved ipd.js to ${TMP_DIR_NAME}/javascript/_/$(cat version.txt)/ipd.js" || true
+CSS_VERSIONED_DIR="${TMP_DIR_NAME}/css/_/${VERSION}"
+JS_VERSIONED_DIR="${TMP_DIR_NAME}/javascript/_/${VERSION}"
 
-for FILNM in $(find ./web -name "*.css" -not -name "*.min.css"); do minify "$FILNM" >"${FILNM%.*}.min.css"; done
+mkdir -p "${CSS_VERSIONED_DIR}" "${JS_VERSIONED_DIR}"
+
+mv "${TMP_DIR_NAME}/css/style.css" "${CSS_VERSIONED_DIR}/style.css" 2>/dev/null
+echo "Moved style.css to ${CSS_VERSIONED_DIR}/style.css"
+
+mv "${TMP_DIR_NAME}/javascript/ipd.js" "${JS_VERSIONED_DIR}/ipd.js" 2>/dev/null
+echo "Moved ipd.js to ${JS_VERSIONED_DIR}/ipd.js"
+
+# Minify CSS and JS files
+find "${TMP_DIR_NAME}" \( -name "*.css" -not -name "*.min.css" \) -exec sh -c 'minify "$1" > "${1%.*}.min.css"' _ {} \;
 echo "Minified CSS files"
 
-for FILNM in $(find ./web -name "*.js" -not -name "*.min.js"); do minify "$FILNM" >"${FILNM%.*}.min.js"; done
+find "${TMP_DIR_NAME}" \( -name "*.js" -not -name "*.min.js" \) -exec sh -c 'minify "$1" > "${1%.*}.min.js"' _ {} \;
 echo "Minified JS files"
 
-wget https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static/video/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4 -O "${TMP_DIR_NAME}/video/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4"
-wget https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static/images/backgrounds/iomp/ivobl-ph-il-5.jpeg -O "${TMP_DIR_NAME}/images/backgrounds/iomp/ivobl-ph-il-5.jpeg"
-wget https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static/images/backgrounds/iomp/ivobl-ph-il-6.jpeg -O "${TMP_DIR_NAME}/images/backgrounds/iomp/ivobl-ph-il-6.jpeg"
-wget https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static/images/backgrounds/iomp/ivobl-ph-il-7.jpeg -O "${TMP_DIR_NAME}/images/backgrounds/iomp/ivobl-ph-il-7.jpeg"
-wget https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static/images/backgrounds/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg -O "${TMP_DIR_NAME}/images/backgrounds/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg"
-echo "Downloaded large videos and images"
+# BASE_URL="https://gl.vprw.ru/tnaudc/thats_a_nice_argument_unfortunately_dot_com-website/-/raw/main/web/static"
+#
+# wget "${BASE_URL}/video/caterpillar-linus-tnaudc-meme-recreation-1080p60fps.mp4" -P "${TMP_DIR_NAME}/video/"
+# wget "${BASE_URL}/images/backgrounds/iomp/ivobl-ph-il-5.jpeg" -P "${TMP_DIR_NAME}/images/backgrounds/iomp/"
+# wget "${BASE_URL}/images/backgrounds/iomp/ivobl-ph-il-6.jpeg" -P "${TMP_DIR_NAME}/images/backgrounds/iomp/"
+# wget "${BASE_URL}/images/backgrounds/iomp/ivobl-ph-il-7.jpeg" -P "${TMP_DIR_NAME}/images/backgrounds/iomp/"
+# wget "${BASE_URL}/images/backgrounds/mksdnr-water-mantaflow-tst/mtlqbp-mntflw-tt-6.jpeg" -P "${TMP_DIR_NAME}/images/backgrounds/mksdnr-water-mantaflow-tst/"
+# echo "Downloaded large videos and images"
 
 rclone --config rclone.conf copy "${TMP_DIR_NAME}/" slctl-sttctnaudc:static_thatsaniceargumentunfortunately_com-7e358dffde76
 echo "Pushed to Selectel Storage"
